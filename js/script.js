4e70464a97d345c82cd674d190e6df7f6ede223a
Lucas <lucas.levi.vaz@gmail.com>
2022-12-06 18:53:36 -0300

    pronto

--- js/script.js
+++ js/script.js
@@ -1,23 +1,41 @@
 document.addEventListener('DOMContentLoaded', function (event) {
 
-    caixa1 = document.querySelectorAll('.caixa1')
+    const caixas = document.querySelectorAll('.caixa1')
     
-    i = 0
-    enviar.addEventListener('click', function(event) {
-        if (i == 0){
-            caixa1.innerHTML = "✅"
-            i = 1
-        }
-        if ( i == 1){
-            caixa1.innerHTML = "🔲"
-            i = 0
-        }
-        
+    caixas.forEach(caixa1 => {
+        a = 0
+        caixa1.addEventListener('click', function(event) {
+            if (a == 0){
+                caixa1.innerHTML = "✅"
+                a = 1
+            }
+            else{
+                caixa1.innerHTML = "🔲"
+                a = 0
+            }
+            console.log('fifiwf')
 
-  
+    
+        })
     })
 
 
+    const certinhos = document.querySelectorAll('.certinho')
+    
+    certinhos.forEach(certinho => {
+        b = 0
+        certinho.addEventListener('click', function(event) {
+            if (b == 0){
+                certinho.innerHTML = "✅"
+                b = 1
+            }
+            else{
+                certinho.innerHTML = "🔲"
+                b = 0
+            }
+            console.log('fifiwf')
 
-
+    
+        })
+    })
 })
