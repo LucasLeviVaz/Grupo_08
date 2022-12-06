4fd86af7a212673a0ec5602f2df85f626c9569a7
Thiago Naves <thiagocn1@al.insper.edu.br>
2022-12-06 17:25:54 -0300

    .

--- js/script.js
+++ js/script.js
@@ -1,10 +1,20 @@
 document.addEventListener('DOMContentLoaded', function (event) {
-    certinho = document.querySelector('.certinho')
-    
-    enviar.addEventListener('click', function(event) {
-  
 
-        event.preventDefault()
+    caixa1 = document.querySelectorAll('.caixa1')
+    
+    i = 0
+    enviar.addEventListener('click', function(event) {
+        if (i == 0){
+            caixa1.innerHTML = "✅"
+            i = 1
+        }
+        if ( i == 1){
+            caixa1.innerHTML = "🔲"
+            i = 0
+        }
+        
+
+  
     })
 
 
